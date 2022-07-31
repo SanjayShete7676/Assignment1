7a1d83f7e0ef105fbc96f3bf3d4ca800192125a3
SanjayShete7676 <sanjayshete7676@gmail.com>
2022-07-31 21:08:23 +0530

    First Commit

---
+++ AsciiVlaue.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class AsciiVlaue {
+
+	public static void main(String[] args) {
+		char alphabet = 'A';
+		int asciiVal = alphabet;
+		System.out.println("ASCII value of "+alphabet+" is "+asciiVal);
+
+	}
+
+}
