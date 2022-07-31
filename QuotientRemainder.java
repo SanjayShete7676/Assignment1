f0ae4967a8ebeb09f2009213a26e278383278800
SanjayShete7676 <sanjayshete7676@gmail.com>
2022-07-31 22:09:52 +0530

    Branch1 Commit

---
+++ QuotientRemainder.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		int divident = 21;
+		int divisor = 2;
+		int quotient = divident/divisor;
+		int reminder = divident%divisor;
+		System.out.println("quotient="+quotient);
+		System.out.println("reminder="+reminder);
+
+	}
+
+}
