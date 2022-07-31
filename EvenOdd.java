ed2c66a41bc207860ac8ae7be322b4e09d954170
SanjayShete7676 <sanjayshete7676@gmail.com>
2022-07-31 21:11:29 +0530

    Second Commit

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int num1 = num%2;
+		if(num1==1) {
+			System.out.println("given no is odd");
+		}else {System.out.println("given no is even");}
+
+	}
+
+}
